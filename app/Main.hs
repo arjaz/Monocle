05a81234db473ff450467f1c2fe04d34dbacac3c
Arjaz <arjaz@protonmail.com>
2022-06-12 11:42:31 +0300

    Handle CORS

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Data.Maybe                     ( maybe )
+import           Network.Wai.Middleware.Cors
 import           Server
 import           System.Environment             ( lookupEnv )
 import           Web.Scotty
 
+apiCors :: CorsResourcePolicy
+apiCors = simpleCorsResourcePolicy
+  { corsOrigins        = Nothing
+  , corsMethods        = simpleMethods <> ["OPTIONS"]
+  , corsRequestHeaders = simpleHeaders <> ["Content-Type", "Authorization"]
+  }
+
+
 main :: IO ()
 main = do
   port <- maybe 3000 read <$> lookupEnv "PORT"
-  scotty port api
+  scotty port $ do
+    middleware (cors (const $ Just apiCors))
+    api
